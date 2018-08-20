87a9eed2e8422b7e8b31b786fb11283862ca85cd
Jacob Zimmer <zimmer.jacob@gmail.com>
2018-08-20 14:03:05 -0400

    addded utils.R

---
+++ Topics Telephone/src/helpers/utils.R
@@ -0,0 +1,156 @@
+### utils.R
+### Jacob Zimmer
+### Tools for working with the output of Topics Telephone experiments
+
+parse.files <- function(file1, file2) {
+  load(file1)
+  t1 <- data.frame(theta)
+  names(t1) <- unlist(mantitles)
+  row.names(t1) <- titles
+
+  load(file2)
+  t2 <- data.frame(theta)
+  if (dim(t2)[2] <= dim(t1)[2]) {names(t2) <- unlist(mantitles)}
+  row.names(t2) <- titles
+  
+  return(list(t1=t1, t2=t2))
+}
+
+filter.models <- function(dfs, selected_topics) {
+  x1 <- dfs[['t1']][names(dfs[['t1']]) %in% selected_topics]
+  x2 <- dfs[['t2']][names(dfs[['t2']]) %in% selected_topics]
+  y1 <- rowSums(dfs[['t1']][ ! (names(dfs[['t1']]) %in% selected_topics)])
+  y2 <- rowSums(dfs[['t2']][ ! (names(dfs[['t2']]) %in% selected_topics)])
+  
+  dfs[['t1']] <- cbind(x1, "Other"=y1)
+  dfs[['t2']] <- cbind(x2, "Other"=y2)
+  dfs[['t1']] <- dfs[['t1']][, order(names(dfs[['t1']]))]
+  dfs[['t2']] <- dfs[['t2']][, order(names(dfs[['t2']]))]
+  
+  return(dfs)
+}
+
+word.count <- function(str1) {
+  return(sapply(gregexpr("[[:alpha:]]+", str1), function(x) sum(x > 0)))
+}
+
+mad <- function(t1, t2, raw_docs=FALSE, raw_tops=FALSE) {
+  t1 <- unname(as.matrix(t1))
+  t2 <- unname(as.matrix(t2))
+
+  if (raw_docs && raw_tops) {
+    print("Arguments are exclusive")
+    return(-1)
+  }
+
+  if (raw_docs) {
+    return(rowMeans(abs(t1-t2)))
+  }
+  if (raw_tops) {
+    return(colMeans(abs(t1-t2)))
+  }
+  
+  return(mean(colMeans(abs(t1-t2))))
+}
+
+mse <- function(t1, t2, raw_docs=FALSE, raw_tops=FALSE) {
+  t1 <- unname(as.matrix(t1))
+  t2 <- unname(as.matrix(t2))
+
+  if (raw_docs && raw_tops) {
+    print("Arguments are exclusive")
+    return(-1)
+  }
+
+  if (raw_docs) {
+    return(rowMeans((t1-t2)*(t1-t2)))
+  }
+  if (raw_tops) {
+    return(colMeans((t1-t2)*(t1-t2)))
+  }
+  
+  return(mean(colMeans((t1-t2)*(t1-t2))))
+}
+
+roc <- function(t1, t2, raw_docs=FALSE) {
+  N <- dim(t1)[1]
+  k <- dim(t1)[2]
+
+  res <- sapply(1:N, function(i) {
+    x <- t1[i,]; y <- t2[i,]
+
+    a <- x[order(x, decreasing = T)][1:k]
+    b <- y[order(y, decreasing = T)][1:k]
+  
+    # get numeric ranks
+    l <- unique(c(as.character(names(a)), as.character(names(b))))
+    a <- as.numeric(factor(names(a), levels=l))
+    b <- as.numeric(factor(names(b), levels=l))
+
+    return(unname(cor.test(a, b, method="spearman")[["estimate"]]))
+  })
+
+  if (raw_docs) {
+    return(res)
+  }
+  
+  return(mean(res))
+}
+
+get.worst.docs <- function(n, t1, t2, type="mad") {
+  if (type == "mad") {
+    dist <- mad(t1, t2, raw_docs=T); names(mad_dist) <- row.names(t1)
+  } else if (type == "roc") {
+    dist <- roc(t1, t2, raw_docs=T); names(mad_dist) <- row.names(t1)
+  } else if (type == "mse") {
+    dist <- mse(t1, t2, raw_docs=T); names(mad_dist) <- row.names(t1)
+  }
+  return(dist[1:n])
+}
+
+print.worst.docs <- function(n, t1, t2) {
+  mad_dist <- mad(t1, t2, raw_docs=T); names(mad_dist) <- row.names(t1)
+  roc_dist <- roc(t1, t2, raw_docs=T); names(roc_dist) <- row.names(t1)
+
+  dat <- data.frame("mad_doc"=names(sort(mad_dist, decreasing = T)),
+                    "mad"=sort(mad_dist, decreasing = T),
+                    "roc_doc"=names(sort(roc_dist, decreasing = F)),
+                    "roc"=sort(roc_dist, decreasing = F))
+
+  for (i in 1:n) {
+    cat(sprintf("%d \t %15s \t %.4f \t %15s \t %.4f \n", i,
+                substr(dat$mad_doc[i], start=1, stop=15), dat$mad[i], 
+                substr(dat$roc_doc[i], start=1, stop=15), dat$roc[i]))
+  }
+}
+
+print.assignments <- function(query, t1, t2) {
+  idx <- match(query, row.names(t1))
+  t1_dist <- t1[idx,]; row.names(t1_dist) <- c()
+  t2_dist <- t2[idx,]; row.names(t2_dist) <- c()
+  
+  k <- dim(t1)[2]
+  mad_score <- mad(t1, t2, raw_docs=T)[idx]
+  roc_score <- roc(t1, t2, raw_docs=T)[idx]
+  
+  cat(sprintf("Document Title: %s\n", query))
+  
+  cat(sprintf("Sample Text: "))
+  filename <- paste("~/Desktop/Topic Aggregation Analysis/Topics Telephone/dat/corpora/ex5/", 
+                    str_replace(query, " ", "_"), sep="")
+  file_text <- readChar(filename, ifelse(file.info(filename)$size > 2000, 2000, file.info(filename)$size))
+  cat(sprintf("%s\n\n", str_squish(file_text)))
+  
+  cat(sprintf("Error metrics:\t\tMAD = %.4f\tROC = %.4f\n\n", mad_score, roc_score))
+  cat(sprintf("Document-Topic Proportions:\n"))
+  cat(sprintf("Topic Label\t\tOriginal\tReconstruction\n"))
+  
+  k <- length(t1_dist)
+  for (i in 1:k) {
+    docname <- names(t1_dist)[i] %>% str_trunc(15) %>% str_pad(15, "right")
+    cat(sprintf("%s\t\t%.4f\t\t%.4f\n", docname, unname(t1_dist)[i], unname(t2_dist)[i]))
+  }
+}
+
+
+
---
+++ Topics Telephone/src/helpers/utils_optimize.R
@@ -0,0 +1,195 @@
+### utils_optimize.R
+### Jacob Zimmer
+### Tools for building an optimal reconstrution and testing stability
+
+library(clue)
+
+merge.reconstruction <- function(m, dfs) {
+  t1 <- dfs[['t1']]; t2 <- dfs[['t2']]
+  t1 <- unname(as.matrix(t1)); t2 <- unname(as.matrix(t2))
+  m <- m[1:dim(t2)[2]]
+  
+  t3 <- t(rowsum(t(t2), m))
+  
+  t3 <- data.frame(t3)
+  names(t3) <- names(dfs[['t1']])
+  row.names(t3) <- row.names(dfs[['t1']])
+  
+  dfs[['t3']] <- t3
+  return(dfs)
+}
+
+create_cost_matrix <- function(dfs, expand=FALSE, stability_test=FALSE) {
+  t1 <- dfs[['t1']]
+  t2 <- dfs[['t2']]
+
+  N     <- dim(t1)[1]
+  k_agg <- dim(t1)[2]
+  k_raw <- dim(t2)[2]
+  
+  print(paste0("Creating ", k_agg, " by ", k_raw, " cost matrix (MSE)..."))
+  cost_mat <- matrix(data = 0, nrow = k_agg, ncol = k_raw)
+  for(i in 1:k_agg){
+    for (j in 1:k_raw) {
+      cost_mat[i,j] <- mean(abs(t1[,i] - t2[,j]))
+    }
+  }
+  
+  #replicate columns for multiple assignments in matching
+  if (expand) {
+    print(paste0("Modifying to ", k_raw, " by ", k_agg*k_raw, " for multiple assignment matching..."))
+    cost_mat <- t(cost_mat)
+    cost_mat <- do.call(cbind, replicate(k_raw, cost_mat, simplify=FALSE))
+  }
+  if (stability_test) {
+    cost_mat <- t(cost_mat)
+  }
+
+  dfs[['c']] <- cost_mat
+  
+  return(dfs)
+}
+
+subsample_solve <- function(lst, cutoff=1000, prop=1) {
+  
+  c <- lst[["c"]]
+  k_agg <- dim(lst[["t1"]])[2]
+  k_raw <- dim(lst[["t2"]])[2]
+  size <- k_agg*prop
+  
+  m <- matrix(data = 0, nrow = k_raw, ncol = k_agg)
+  s <- sum(rowSums(m) > cutoff)
+  
+  i <- 0
+  while (s < k_raw) {
+    # subsample columns into small cost matrix
+    cols <- sample(1:ncol(c), size, replace = FALSE) # [1:12]
+    samp <- c[,cols]   #[1:12, 1:12]
+    
+    # apply solve_LSAP
+    sol <- solve_LSAP(samp)[1:k_agg] #[1:12]
+    
+    # sorts cols and subsets
+    row_idx <- cols[sol]
+    col_idx <- c(1:k_agg)
+    
+    # new way...
+    m[row_idx + nrow(m)*(col_idx-1)] <- m[row_idx + nrow(m)*(col_idx-1)] + 1
+    
+    # exit conditions
+    s <- sum(rowSums(m) > cutoff)
+    if (i %% 10000 == 0) {
+      print(sprintf("%7d iterations: %6d done, max: %6d, min: %d", i, s, max(rowSums(m)), min(rowSums(m))))
+    }
+    i <- i + 1
+    
+    if (max(rowSums(m)) > 10 * cutoff) {
+      print(sprintf("Warning: cutoff not reached, min = %d", min(rowSums(m))))
+      break
+    }
+  }
+  print(sprintf("%7d iterations: %3d rows satisfied", i, s))
+  
+  return(apply(m, 1, which.max))
+}
+
+random_search <- function(lst, iter=1000) {
+  
+  c <- lst[["c"]]
+  t1 <- lst[["t1"]]
+  t2 <- lst[["t2"]]
+  k_agg <- dim(t1)[2]
+  k_raw <- dim(t2)[2]
+  
+  m <- matrix(data = 0, nrow = k_raw, ncol = 0)
+  scores <- c()
+  best <- 100
+  
+  i <- 0
+  t3 <- t(t2)
+  while (i < iter) {
+    # randomly assign topics in t2
+    assignments <- sample(1:k_agg, k_raw, replace = TRUE)
+    
+    # condense t2 by topic
+    t4 <- rowsum(t3, assignments)
+    
+    score  <- mad(t1, t(t4))
+    if (score < best) {
+      best <- score
+      m <- assignments
+    }
+    
+    if (i %% 100 == 0) {
+      print(sprintf("%5d iterations complete", i))
+    }
+    i <- i+1
+  }
+  
+  return(m)
+}
+
+priority_solve <- function(file1, file2) {
+  
+  lst <- create_cost_matrix(file1, file2)
+  
+  while (dim(lst[["t2"]])[2] > 0) {
+    
+    c <- lst[["c"]]
+    t1 <- lst[["t1"]]
+    t2 <- lst[["t2"]]
+    k_agg <- lst[["k_agg"]]
+    k_raw <- lst[["k_raw"]]
+    
+    # run matching with k_agg (returns in K' sorted order 1:12)
+    if (dim(c)[1] > dim(c)[2]) {
+      tmp <- solve_LSAP(t(c))[1:dim(c)[2]] #assignments 
+      
+      # update t1 and break (last iter)
+      for (i in 1:k_agg) {
+        #if (all(t1[,tmp[i]] - t2[,i] >= 0)) {
+          t1[,tmp[i]] <- t1[,tmp[i]] - t2[,i]
+        # } else {
+        #   print("BUG")
+        #   tmp[-i]
+        # }
+      }
+      t1 <- abs(t1)
+      break
+    } else {
+      tmp <- solve_LSAP(c)[1:k_agg]
+    }
+    
+    # adjust k_agg topics in t1 to subtract matched t2 cols
+    # for column in t2[,tmp] subtract from corresponding col in t1
+    for (i in 1:k_agg) {
+      if (all(t1[,i] - t2[,tmp[i]] >= 0)) {
+        t1[,i] <- t1[,i] - t2[,tmp[i]]
+      } else {
+        tmp[-i]
+      }
+    }
+    t2 <- t2[,-tmp]
+    
+    # rerun create new cost matrix from t1 and t2
+    N <- dim(t1)[1]
+    k_raw <- k_raw - length(tmp)
+    
+    if (k_raw == 0) {break}
+    print(paste0("Creating ", k_agg, " by ", k_raw, " cost matrix (MSE)..."))
+    cost_mat <- matrix(data = 0, nrow = k_agg, ncol = k_raw)
+    for(i in 1:k_agg){
+      for (j in 1:k_raw) {
+        cost_mat[i,j] <- mean((t1[,i] - t2[,j])^2)
+      }
+    }
+    
+    lst = list("c"=cost_mat, "t1"=t1, "t2"=t2, "N"=N, "k_agg"=k_agg, "k_raw"=k_raw)
+  }
+  
+  m_a_d <- mean(apply(   t1, 2, mean))
+  m_s_e <- mean(apply(t1*t1, 2, mean))
+  
+  return(list("mad"=m_a_d, "mse"=m_s_e))
+}
+
